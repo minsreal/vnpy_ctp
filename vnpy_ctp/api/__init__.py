a7fba5ae7fce829ab509f42490a217ed9330eabc
chenlm <353879649@qq.com>
2026-01-17 10:54:28 +0800

    fix build

--- vnpy_ctp/api/__init__.py
+++ vnpy_ctp/api/__init__.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+# 在 Windows 上，添加 DLL 搜索路径
+if sys.platform == 'win32':
+    api_dir = os.path.dirname(os.path.abspath(__file__))
+    os.add_dll_directory(api_dir)
+
 from .vnctpmd import MdApi      # noqa
 from .vnctptd import TdApi      # noqa
 from .ctp_constant import *     # noqa
