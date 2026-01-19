9a6017993170138ff94cc89ccf63b845400cc9aa
chenlm <353879649@qq.com>
2026-01-19 22:19:38 +0800

    add gitignore

--- vnpy_ctp/api/__init__.py
+++ vnpy_ctp/api/__init__.py
@@ -1,10 +1,3 @@
-import os
-import sys
-
-# 在 Windows 上，添加 DLL 搜索路径
-if sys.platform == 'win32':
-    api_dir = os.path.dirname(os.path.abspath(__file__))
-    os.add_dll_directory(api_dir)
 
 from .vnctpmd import MdApi      # noqa
 from .vnctptd import TdApi      # noqa
